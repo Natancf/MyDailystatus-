9782479b43e6dac7c541b4cbe53d0fad7ca0563f
Natanael Costa <natancf10@gmail.com>
2020-04-19 00:13:12 -0300

    Primeira parte do mydailystatus

---
+++ .next/static/webpack/static/development/pages/_app.js.a938ea5dee0010e468fe.hot-update.js
@@ -0,0 +1,51 @@
+webpackHotUpdate("static\\development\\pages\\_app.js",{
+
+/***/ "./components/Footer.js":
+/*!******************************!*\
+  !*** ./components/Footer.js ***!
+  \******************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+var _this = undefined,
+    _jsxFileName = "C:\\mydailystatus\\components\\Footer.js";
+
+var __jsx = react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement;
+
+
+var Footer = function Footer() {
+  return __jsx("div", {
+    __self: _this,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 5,
+      columnNumber: 9
+    }
+  }, "MyDailystatus \xE9 um projeto criado durante o Fullstack Lab do DevPleno. ", __jsx("br", {
+    __self: _this,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 6,
+      columnNumber: 84
+    }
+  }), "Implementado por: ", __jsx("a", {
+    href: "https://github.com/Natancf",
+    __self: _this,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 7,
+      columnNumber: 31
+    }
+  }, "Natanael Costa"));
+};
+
+/* harmony default export */ __webpack_exports__["default"] = (Footer);
+
+/***/ })
+
+})
+//# sourceMappingURL=_app.js.a938ea5dee0010e468fe.hot-update.js.map
---
+++ components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <div className='py-4 text-center bg-gray-400'>
+            MyDailystatus é um projeto criado durante o Fullstack Lab do DevPleno. <br/>
+            Implementado por: {' '}
+            <a href='https://github.com/Natancf'>Natanael Costa</a>
+        </div>
+    )
+}
+export default Footer 
